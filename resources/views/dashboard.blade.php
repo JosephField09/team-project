efa74578a333e4b59d8a6d032bf5cab687071603
usmaan04 <usmaanahmed2004@gmail.com>
2024-11-23 15:51:17 +0000

    dashboard started

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta tags, title, CSS and JS -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/favicon.png') }}">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+
+<!-- Header Section -->
+<section id="header">
+    <nav>
+        <div class="navbar-left">
+            <a href="{{ route('home') }}"><img src="{{ asset('assets/E-spresso_logo.jpg') }}"></a>
+        </div>
+        <div class="navbar-middle">
+            <a class="middle" href="{{ route('home') }}">Home</a>
+            <a class="middle" href="{{ route('products') }}">Products</a>
+            <a class="middle" href="{{ route('about-us') }}">About Us</a>
+            <a class="middle" href="{{ route('blog') }}">Blog</a>
+        </div>
+        <div class="navbar-right">
+            <a class="account" href="/team-project/resources/views/basket.blade.php"><i class='bx bx-user'></i></a>
+            <a class="basket" href="/team-project/resources/views/basket.blade.php"><i class='bx bx-basket'></i></a>
+        </div>
+    </nav>
+</section>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -5,13 +37,74 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    <form method="POST" action="http://127.0.0.1:8000/logout">
+                            <input type="hidden" name="_token" value="P0AdiQgY9zQbsPmvGkwmhGeVQ4pfIUq2NXfqmJrH" autocomplete="off">
+                            <a class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" href="http://127.0.0.1:8000/logout" onclick="event.preventDefault();
+                                                this.closest('form').submit();">Log Out</a>
+                        </form>
+
+</x-app-layout>
+
+
+<!-- Footer Section -->
+<section id="footer">
+            <footer class="top">
+            <div class="logo-desc-soc">
+                <div class="logo">
+                    <a href="{{ route('home') }}"><img src="{{ asset('assets/E-spresso_logo.jpg') }}"></a>
+                </div>
+                <p class="desc">At E-spresso, we’re passionate about delivering the perfect coffee experience. From premium beans to convenient pods, we offer a selection to satisfy every coffee lover’s taste. Whether you’re a coffee connoisseur or just beginning your journey, Our store is your gateway to a world of rich flavors and aromatic delights.</p>
+                <div class="socials">
+                    <ul class="social-links">
+                        <i class='bx bxl-facebook-circle'></i>
+                        <i class='bx bxl-instagram-alt' ></i>
+                        <i class='bx bxl-linkedin-square' ></i>
+                        <i class='bx bxl-pinterest' ></i>
+                    </ul>
                 </div>
             </div>
-        </div>
-    </div>
-</x-app-layout>
+            <div class="quick-links">
+                <h3>Quick Links</h3>
+                <ul class="links">
+                    <li><a href="{{ route('home') }}">Home</a></li>
+                    <li><a href="{{ route('products') }}">Products</a></li>
+                    <li><a href="{{ route('about-us') }}">About Us </a></li>
+                    <li><a href="{{ route('blog') }}">Blog</a></li>
+                </ul>
+            </div>
+
+            <div class="information">
+                <h3>Information</h3>
+                <ul class="details">
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Terms & Conditions</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+
+            <div class="contact-information">
+                <h3>Contact Info</h3>
+                <ul class="info">
+                    <li>
+                        <i class="bx bx-phone"></i
+                        ><a href="tel:+44 1234 567890">+44 1234 567890</a>
+                    </li>
+                    <li>
+                        <i class="bx bx-envelope"></i
+                        ><a href="mailto:espressoadmin@gmail.com" :
+                        >espressoadmin@gmail.com</a
+                        >
+                    </li>
+                    <li>
+                        <i class="bx bx-building"></i>
+                        <a href="https://maps.app.goo.gl/acBvLWsSNhHqHQcG7">Aston University, Aston St, Birmingham B4 7ET</a>
+                    </li>
+                </ul>
+            </div>
+            </footer>
+            <footer class="bottom">
+            <div class="footer">
+                <p>© E-SPRESSO | All Rights Reserved</p>
+            </div>
+            </footer>
+</section>
