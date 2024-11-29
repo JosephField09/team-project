e2f64c3a0708123f92986429f20750ea11529471
usmaan04 <usmaanahmed2004@gmail.com>
2024-11-29 15:27:45 +0000

    fixed John's category error

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Category;
+
 class CategoryController extends Controller
 {
-    //
+    public function add_category(Request $request)
+    {
+        $category = new Category;
+
+        $category->name = $request->category;
+
+        $category->save();
+
+    }
 }
