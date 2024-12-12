4762931da7a9cef116e110937aee26c41b1bf8be
usmaan04 <usmaanahmed2004@gmail.com>
2024-12-12 17:45:04 +0000

    added dark mode to login and register page

--- resources/views/admin/auth/register.blade.php
+++ resources/views/admin/auth/register.blade.php
@@ -14,6 +14,7 @@
 <body>
     <main>
         <!-- Header Section -->
+        <!-- Header Section -->
         <section id="header">
             <nav id="main">
                 <!-- Left navbar section -->
@@ -22,7 +23,7 @@
                </div>
                <!-- Middle navbar section -->
                 <div class="navbar-middle">
-                    <a class="middle" href="{{ route('home') }}">Home</a>
+                    <a class="middle option-selected" href="{{ route('home') }}">Home</a>
                     <a class="middle" href="{{ route('products') }}">Products</a>
                     <a class="middle" href="{{ route('about-us') }}">About Us</a>
                     <a class="middle" href="{{ route('blogs.index') }}">Blog</a>
@@ -42,6 +43,8 @@
                                     <span class="basket-count">{{ $basketCount }}</span>
                                 @endif
                             </a>
+                            <button id="toggleMode"><i class='bx bxs-moon'></i></button>
+                            <script src="{{ asset('js/dark-mode.js') }}"></script>
                         <!-- If user is user -->
                         @elseif(Auth::user()->userType === 'user')
                             <a class="account" href="{{ route('dashboard') }}">
@@ -53,6 +56,8 @@
                                     <span class="basket-count">{{ $basketCount }}</span>
                                 @endif
                             </a>
+                            <button id="toggleMode"><i class='bx bxs-moon'></i></button>
+                            <script src="{{ asset('js/dark-mode.js') }}"></script>
                         @endif
                     <!-- If user is not logged in -->
                     @else
@@ -61,6 +66,8 @@
                         <a class="basket" href="{{ route('basket') }}">
                             <i class='bx bx-basket'></i>
                         </a>
+                        <button id="toggleMode"><i class='bx bxs-moon'></i></button>
+                        <script src="{{ asset('js/dark-mode.js') }}"></script>
                     @endif
                 </div>
             </nav>
